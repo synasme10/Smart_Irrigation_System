aadfd9d79711f526857bbcc77c13c5016cc0ef59
Sujan Maharjan <serenadesan10@gmail.com>
2019-12-10 14:33:35 +0545

    Irrigation app created

---
+++ app/src/main/java/com/example/smart_irrigation_system/IrrigationApp.java
@@ -0,0 +1,12 @@
+package com.example.smart_irrigation_system;
+
+import android.app.Application;
+
+public class IrrigationApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+}
